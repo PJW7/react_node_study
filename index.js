a35419bbbbf1fea894b38f17ad28500b7a16ff08
pjw <hoontaek.lim@cj.net>
2021-06-18 17:48:42 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ app.use(express.urlencoded({extended: true}));
 //application/json
 app.use(express.json());
 
-
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://pjw:1234@boilerplate.5illy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+const config = require('./config/key');
+
+mongoose.connect(config.mongoURI ,{
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('mongoDB Connected...'))
   .catch(err => console.log(err));//error 안뜨게
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+app.get('/', (req, res) => res.send('Hello, World!!!!!!!!!!!!!!!!'));
 
 app.post('/register',(req, res) => {
   //회원가입할 때 필요한 정보들을 클라이언트에서 가져오면 그것들을 데이터베이스에 넣어준다.
